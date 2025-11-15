df7570039b0ababda46e27aee2cae7c277427bd2
Talia labute <talia.labute@gmail.com>
2025-11-15 15:24:37 -0500

    stuff

--- frontend/ui/src/app/cancer-detection/cancer-detection.ts
+++ frontend/ui/src/app/cancer-detection/cancer-detection.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cancer-detection',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './cancer-detection.html',
+  standalone: true,
   styleUrl: './cancer-detection.css',
 })
 export class CancerDetection {
+  cancerForm: FormGroup;
+  selectedFile: File | null = null;
+  isSubmitting: boolean = false;
 
+  constructor(private fb: FormBuilder) {
+    this.cancerForm = this.fb.group({
+      medicare: ['', Validators.required],
+      photo: [null, [Validators.required]]
+    });
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files ? input.files[0] : null;
+
+    this.selectedFile = file;
+
+    this.cancerForm.patchValue({ photo: file });
+    this.cancerForm.get('photo')?.updateValueAndValidity();
+  }
+
+  onSubmit(): void { //TODO: finish
+    this.cancerForm.markAllAsTouched();
+
+    if (this.cancerForm.invalid || !this.selectedFile) {
+      return; 
+    }
+
+  }
 }
+
+
