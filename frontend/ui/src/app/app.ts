00336538a8b646ea836350cc114f2f9c190b5673
Talia labute <talia.labute@gmail.com>
2025-11-15 15:24:37 -0500

    stuff

--- frontend/ui/src/app/app.ts
+++ frontend/ui/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CancerDetection } from './cancer-detection/cancer-detection';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CancerDetection],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
